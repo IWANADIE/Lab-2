5d48898ccdd8ef703ab328817d601e2f2bd24378
Owen <OWENTAN.21@ichat.sp.edu.sg>
2022-10-28 00:55:20 +0800

    a bit done

---
+++ exercise 4.py
@@ -0,0 +1,13 @@
+def main():
+    calc_average_temperature()
+    calc_min_max_temperature()
+def calc_average_temperature():
+    print("Please enter the temperature to find average: ")
+    x = input()
+    print(x)
+def calc_min_max_temperature():
+    print("Please enter the temperature to find min and max: ")
+    y = input()
+    print(y)
+
+main ()
