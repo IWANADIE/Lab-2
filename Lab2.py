7e473d63c8cf885647cf99cda81b5a3c8f969914
Owen <OWENTAN.21@ichat.sp.edu.sg>
2022-10-27 15:51:00 +0800

    Lab2 exercise2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,12 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python.")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python.")
+    display_main_menu()
+    calc_average()
+
+def display_main_menu():
+    print("display_main_menu")
+
+def calc_average():
+    print("calc_average")
+
+main()
