191f572b9ac3d37fbe152e48b06c5268a2df6852
Owen <OWENTAN.21@ichat.sp.edu.sg>
2022-10-28 01:26:25 +0800

    send help

--- exercise 4.py
+++ exercise 4.py
@@ -1,9 +1,22 @@
+import statistics
 def main():
+    get_user_input()
     calc_average_temperature()
-    calc_min_max_temperature()
-def calc_average_temperature():
-    print("Please enter the temperature to find average: ")
+    #calc_min_max_temperature()
+
+def get_user_input():
     x = input()
+    print("You entered: " + x)
+    y = x.split(",")
+    print(y)
+    mylist = y
+    print(mylist)
+    z = list(map(float, mylist))
+    print(z)
+
+    return z
+def calc_average_temperature(num):
+    x = sum(num)/len(num)
     print(x)
 def calc_min_max_temperature():
     print("Please enter the temperature to find min and max: ")
